1aa468d991c7643a5a6021c00ef3774403352b8b
ashton <Ashtonsisson@gmail.com>
2020-11-12 14:25:46 -0500

    Fixed the repo
    I had a 100mb plus zip in it and it broke the repo so I redid it with all the files but several commits were lost but not the file in them

---
+++ JavaScript Hands on/IndividualCase/scripts/index.js
@@ -0,0 +1,59 @@
+function compare() {
+  let suit1 = document.getElementById('suit1').value
+  let suit2 = document.getElementById('suit2').value
+  let output = ""
+
+  if (suit1 == 't45') {
+    suit1 = {
+      name: "T-45",
+      bd: 500,
+      ed: 310,
+    }
+  } else if (suit1 == 't51') {
+    suit1 = {
+      name: "T-51",
+      bd: 740,
+      ed: 490,
+    }
+  } else {
+    suit1 = {
+      name: "T-60",
+      bd: 980,
+      ed: 645,
+    }
+  }
+
+  if (suit2 == 't45') {
+    suit2 = {
+      name: "T-45",
+      bd: 500,
+      ed: 310,
+    }
+  } else if (suit2 == 't51') {
+    suit2 = {
+      name: "T-51",
+      bd: 740,
+      ed: 490,
+    }
+  } else {
+    suit2 = {
+      name: "T-60",
+      bd: 980,
+      ed: 645,
+    }
+  }
+  console.log(suit1.name);
+  console.log(suit1.bd);
+
+  let bdDif = suit1.bd - suit2.bd
+
+  if (bdDif > 0) {
+    output += suit1.name + " has " + bdDif + " more balsitic defence than " + suit2.name
+  } else if (bdDif == 0) {
+    output += suit1.name + " has the same balsitic defence as " + suit2.name
+  } else if (bdDif < 0) {
+    output += suit1.name + " has " + Math.abs(bdDif) + " less balsitic defence than " + suit2.name
+  }
+
+  document.getElementById("output").innerHTML = output
+}
